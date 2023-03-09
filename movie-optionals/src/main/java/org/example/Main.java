ab0fabefa8f3e24373a16787c6394883e155b772
Neel Doshi <neel.doshi@sky.uk>
2023-03-09 12:00:39 +0000

    add Movie Service class
    Co-authored-by: Anadibya Rai <anadibya.rai@sky.uk>

--- movie-optionals/src/main/java/org/example/Main.java
+++ movie-optionals/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 public class Main {
+    public static MovieService movieService = new MovieService();
+
     public static void main(String[] args) {
 
         Movie movie = findMovieByTitle("spiderman");
@@ -8,15 +10,19 @@ public class Main {
         System.out.println(movie.getTitle());
     }
 
-    /**
-     * Method to find and return a movie by a title search string
-     *
-     * @param title the title search string
-     * @return Movie object
-     */
-    private static Movie findMovieByTitle(String title) {
+    public static Movie findMovieByTitle(String title) {
+        Movie movie = movieService.findMovieByTitle(title);
+        return movie;
+    }
+
+}
+
+class MovieService {
+
+    public Movie findMovieByTitle(String title) {
         Movie movie = new Movie("spiderman", 130);
         return movie;
     }
+
 
 }
